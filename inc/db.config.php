c36efdaa27d50920276487c4bb8cde2015b9e40e
daoos <igeeker@126.com>
2015-12-30 18:21:34 +0800

    Update db.config.php

--- inc/db.config.php
+++ inc/db.config.php
@@ -2,7 +2,7 @@
 	//数据库连接配置信息
 	$db_hoorayos_config = array(
 		'dsn'=>'mysql:host=localhost;dbname=hoorayos',
-		'name'=>'root',
-		'password'=>'hooray'
+		'name'=>'adminZnQND1X',
+		'password'=>'yk6fgIwFf6xu'
 	);
 ?>
